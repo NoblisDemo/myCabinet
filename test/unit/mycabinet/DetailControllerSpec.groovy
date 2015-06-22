dc98bff5ce85533ed7feef559918f5b597bbec07
vburmaka <sev.burmaka@noblis.org>
2015-06-22 11:19:43 -0400

    created detailview controller

---
+++ test/unit/mycabinet/DetailControllerSpec.groovy
@@ -0,0 +1,20 @@
+package mycabinet
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(DetailController)
+class DetailControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
