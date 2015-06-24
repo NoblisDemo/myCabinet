b642a2f87fab6fd1da09e08243f84382695fb306
Blek <m29792@1629695a-m29792.mitretek.org>
2015-06-24 10:07:19 -0400

    Fix bunked merge

--- grails-app/controllers/org/noblis/myHealthAlerts/HealthProductController.groovy
+++ grails-app/controllers/org/noblis/myHealthAlerts/HealthProductController.groovy
@@ -1,10 +1,7 @@
 package org.noblis.myHealthAlerts
 
-<<<<<<< HEAD
 import org.noblis.myHealthAlerts.HealthProduct
 
-=======
->>>>>>> 17f515c5af4fc8d36da90f6f65ba60b12dcb2c0a
 class HealthProductController {
 
     def springSecurityService
@@ -14,17 +11,7 @@ class HealthProductController {
     }
 
     def add() {
-<<<<<<< HEAD
         def product = new HealthProduct(params)
-=======
-        def product = new HealthProduct(productName: params.productName)
-        if(params.startDate){
-            product.startDate = new Date().parse("MM/dd/yyyy", params.startDate)
-        }
-        if(params.endDate){
-            product.endDate = new Date().parse("MM/dd/yyyy", params.endDate)
-        }
->>>>>>> 17f515c5af4fc8d36da90f6f65ba60b12dcb2c0a
         if(product.validate()){
             product.save()
             //get current user and add to their list of products
@@ -42,17 +29,7 @@ class HealthProductController {
     def update() {
         def product = HealthProduct.get(params.id)
         if(product){
-<<<<<<< HEAD
             product.properties = params
-=======
-            product.productName = params.productName
-            if(params.startDate){
-                product.startDate = new Date().parse("MM/dd/yyyy", params.startDate)
-            }
-            if(params.endDate){
-                product.endDate = new Date().parse("MM/dd/yyyy", params.endDate)
-            }
->>>>>>> 17f515c5af4fc8d36da90f6f65ba60b12dcb2c0a
             if(product.validate()){
                 product.save()
                 render status:200
@@ -78,11 +55,7 @@ class HealthProductController {
                 product.delete(flush:true)
                 render status: 200
             } catch (org.springframework.dao.DataIntegrityViolationException e) {
-<<<<<<< HEAD
                 flash.message = "Could not delete product ${product.name}"
-=======
-                flash.message = "Could not delete product ${product.productName}"
->>>>>>> 17f515c5af4fc8d36da90f6f65ba60b12dcb2c0a
                 render status:417
             }
         }
