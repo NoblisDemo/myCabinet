c3cbb3d266458c1867461e8fcc9284612679d8c5
Evan Thaler <evan.thaler@noblis.org>
2015-06-24 10:04:38 -0400

    Codenarc'd

--- grails-app/domain/org/noblis/myHealthAlerts/HealthProduct.groovy
+++ grails-app/domain/org/noblis/myHealthAlerts/HealthProduct.groovy
@@ -1,5 +1,10 @@
 package org.noblis.myHealthAlerts
 
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+@ToString(includeNames = true, includeFields = true)
+@EqualsAndHashCode
 class HealthProduct {
 
     String productName
