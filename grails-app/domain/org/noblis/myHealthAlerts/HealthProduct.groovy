dc7ee993e7aa8d3955a95b7455178eab73fbe45d
vburmaka <sev.burmaka@noblis.org>
2015-06-25 14:44:53 -0400

    adding duplicate product names bug fix

--- grails-app/domain/org/noblis/myHealthAlerts/HealthProduct.groovy
+++ grails-app/domain/org/noblis/myHealthAlerts/HealthProduct.groovy
@@ -11,7 +11,7 @@ class HealthProduct {
     Boolean emailNotification = true
 
     static constraints = {
-        productName blank:false
+        productName blank:false, unique: true
         startDate nullable:true
         endDate nullable:true
     }
